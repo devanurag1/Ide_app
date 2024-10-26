ff709ba34d9a4d32fa7771eaf420d45c2789b77d
anuragsingh011 <anuraggit100@gmail.com>
2024-10-26 12:39:23 +0530

    complete code editor app . here you can write you code of 5 diffrent languages

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,120 @@
+import 'package:code_text_field/code_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_highlight/themes/monokai-sublime.dart';
+import 'package:highlight/languages/dart.dart' as dart;
+import 'package:highlight/languages/java.dart' as java;
+import 'package:highlight/languages/cpp.dart' as cpp;
+import 'package:highlight/languages/python.dart' as python;
+import 'package:highlight/languages/javascript.dart' as javascript;
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: CodeEditor(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CodeEditor extends StatefulWidget {
+  const CodeEditor({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  State<CodeEditor> createState() => _CodeEditorState();
+}
+
+class _CodeEditorState extends State<CodeEditor> {
+  CodeController? _codeController;
+  String selectedLanguage = 'Dart';
+
+  final Map<String, dynamic> languages = {
+    'Dart': {
+      'highlighter': dart.dart,
+      'sampleCode': 'void main() {\n    print("Hello, Dart!");\n}'
+    },
+    'Java': {
+      'highlighter': java.java,
+      'sampleCode':
+          'public class Main {\n    public static void main(String[] args) {\n        System.out.println("Hello, Java!");\n    }\n}'
+    },
+    'C++': {
+      'highlighter': cpp.cpp,
+      'sampleCode':
+          '#include <iostream>\nint main() {\n    std::cout << "Hello, C++!";\n    return 0;\n}'
+    },
+    'Python': {
+      'highlighter': python.python,
+      'sampleCode': 'def main():\n    print("Hello, Python!")\n\nmain()'
+    },
+    'JavaScript': {
+      'highlighter': javascript.javascript,
+      'sampleCode':
+          'function main() {\n    console.log("Hello, JavaScript!");\n}\nmain();'
+    }
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCodeController('Dart'); // Initialize with Dart as default
+  }
+
+  void _initializeCodeController(String language) {
+    _codeController?.dispose();
+
+    final languageConfig = languages[language];
+    _codeController = CodeController(
+      text: languageConfig['sampleCode'],
+      language: languageConfig['highlighter'],
     );
+
+    setState(() {});
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void dispose() {
+    _codeController?.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Code Editor'),
+        foregroundColor: Colors.blue,
+        backgroundColor: Colors.amber,
+        actions: [
+          DropdownButton<String>(
+            value: selectedLanguage,
+            dropdownColor: Colors.amber,
+            icon: const Icon(Icons.arrow_drop_down, color: Colors.white),
+            onChanged: (String? newLanguage) {
+              if (newLanguage != null && newLanguage != selectedLanguage) {
+                selectedLanguage = newLanguage;
+                _initializeCodeController(newLanguage);
+              }
+            },
+            items:
+                languages.keys.map<DropdownMenuItem<String>>((String language) {
+              return DropdownMenuItem<String>(
+                value: language,
+                child:
+                    Text(language, style: const TextStyle(color: Colors.black)),
+              );
+            }).toList(),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: _codeController != null
+              ? CodeField(
+                  controller: _codeController!,
+                  textStyle: const TextStyle(fontFamily: 'SourceCodePro'),
+                )
+              : const CircularProgressIndicator(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
